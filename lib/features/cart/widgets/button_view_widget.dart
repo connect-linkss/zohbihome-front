8a9df2bbda4c6e4a00b4627d8dbb8ef98c92f3af
ZeinaIbrahim <zeinaibra1999@gmail.com>
2024-11-05 15:36:22 +0200

    whatsapp button

--- lib/features/cart/widgets/button_view_widget.dart
+++ lib/features/cart/widgets/button_view_widget.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_declarations, deprecated_member_use
+
+import 'package:url_launcher/url_launcher.dart';
 import 'package:zohbi_user/common/widgets/custom_button_widget.dart';
 import 'package:zohbi_user/features/checkout/providers/checkout_provider.dart';
 import 'package:zohbi_user/features/coupon/providers/coupon_provider.dart';
@@ -10,6 +13,8 @@ import 'package:zohbi_user/utill/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/cart_provider.dart';
+
 class ButtonViewWidget extends StatelessWidget {
   final double itemPrice;
   final double total;
@@ -17,36 +22,115 @@ class ButtonViewWidget extends StatelessWidget {
   final double discount;
 
   const ButtonViewWidget({
-    Key? key, required this.itemPrice,
+    Key? key,
+    required this.itemPrice,
     required this.total,
     required this.deliveryCharge,
     required this.discount,
   }) : super(key: key);
 
+  Future<void> _sendOrderToWhatsApp(BuildContext context) async {
+    final CartProvider cartProvider =
+        Provider.of<CartProvider>(context, listen: false);
+    final CheckoutProvider checkoutProvider =
+        Provider.of<CheckoutProvider>(context, listen: false);
+    final CouponProvider couponProvider =
+        Provider.of<CouponProvider>(context, listen: false);
+
+    String itemPriceString = PriceConverterHelper.convertPrice(itemPrice);
+    String totalString = PriceConverterHelper.convertPrice(total);
+    String deliveryChargeString =
+        PriceConverterHelper.convertPrice(deliveryCharge);
+    String discountString = PriceConverterHelper.convertPrice(discount);
+
+    StringBuffer message = StringBuffer("Order Invoice:\n\n");
+
+    if (cartProvider.cartList.isNotEmpty) {
+      for (var cartItem in cartProvider.cartList) {
+        String productName = cartItem!.product?.name ?? 'Unknown';
+        String productPrice =
+            PriceConverterHelper.convertPrice(cartItem.price ?? 0);
+        int productQuantity = cartItem.quantity ?? 0;
+        String productVariation =
+            cartItem.variation?.map((v) => v.type).join(', ') ?? 'None';
+
+        message.write("Product: $productName\n");
+        message.write("Price: $productPrice\n");
+        message.write("Quantity: $productQuantity\n");
+        message.write("Variation: $productVariation\n\n");
+      }
+    }
+
+    message.write("Item Price: $itemPriceString\n");
+    message.write("Delivery Charge: $deliveryChargeString\n");
+    message.write("Coupon Code: ${couponProvider.coupon?.code ?? 'None'}\n");
+    message.write("Discount: $discountString\n");
+    message.write("Order Type: ${checkoutProvider.orderType}\n");
+    message.write("\n--------------------------------------\n");
+    message.write("Total: $totalString\n");
+
+    final String whatsappUrl =
+        "https://wa.me/96176180943?text=${Uri.encodeComponent(message.toString())}";
+
+    if (await canLaunch(whatsappUrl)) {
+      await launch(whatsappUrl);
+    } else {
+      throw 'Could not launch $whatsappUrl';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    final CheckoutProvider checkoutProvider = Provider.of<CheckoutProvider>(context, listen: false);
-    final CouponProvider couponProvider = Provider.of<CouponProvider>(context, listen: false);
+    final CheckoutProvider checkoutProvider =
+        Provider.of<CheckoutProvider>(context, listen: false);
+    final CouponProvider couponProvider =
+        Provider.of<CouponProvider>(context, listen: false);
 
     return Container(
       width: Dimensions.webScreenWidth,
       padding: const EdgeInsets.all(Dimensions.paddingSizeSmall),
-      child: CustomButtonWidget(
-          btnTxt: getTranslated('proceed_to_checkout', context), onTap: () {
-        if(itemPrice < Provider.of<SplashProvider>(context, listen: false).configModel!.minimumOrderValue!) {
-          showCustomSnackBar('Minimum order amount is ${PriceConverterHelper.convertPrice(Provider.of<SplashProvider>(context, listen: false).configModel!.minimumOrderValue)}, you have ${PriceConverterHelper.convertPrice(itemPrice)} in your cart, please add more item.', context);
-        } else {
-          Navigator.pushNamed(context, Routes.getCheckoutRoute(
-            amount: total,
-            deliveryCharge: deliveryCharge,
-            type: checkoutProvider.orderType,
-            discount: discount,
-            code: couponProvider.coupon?.code,
-            fromCart: true,
-          ));
-        }
-      }),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: CustomButtonWidget(
+              btnTxt: getTranslated('proceed_to_checkout', context),
+              onTap: () {
+                if (itemPrice <
+                    Provider.of<SplashProvider>(context, listen: false)
+                        .configModel!
+                        .minimumOrderValue!) {
+                  showCustomSnackBar(
+                    'Minimum order amount is ${PriceConverterHelper.convertPrice(Provider.of<SplashProvider>(context, listen: false).configModel!.minimumOrderValue)}, you have ${PriceConverterHelper.convertPrice(itemPrice)} in your cart, please add more items.',
+                    context,
+                  );
+                } else {
+                  Navigator.pushNamed(
+                    context,
+                    Routes.getCheckoutRoute(
+                      amount: total,
+                      deliveryCharge: deliveryCharge,
+                      type: checkoutProvider.orderType,
+                      discount: discount,
+                      code: couponProvider.coupon?.code,
+                      fromCart: true,
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+          // WhatsApp Button
+          Expanded(
+            child: CustomButtonWidget(
+              backgroundColor: Colors.green,
+              btnTxt: "Send to WhatsApp",
+              onTap: () => _sendOrderToWhatsApp(context),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/checkout/screens/checkout_screen.dart
+++ lib/features/checkout/screens/checkout_screen.dart
@@ -42,7 +42,8 @@ class CheckoutScreen extends StatefulWidget {
     required this.fromCart,
     required this.discount,
     required this.couponCode,
-    required this.deliveryCharge, this.cartList,
+    required this.deliveryCharge,
+    this.cartList,
   }) : super(key: key);
 
   @override
@@ -50,39 +51,49 @@ class CheckoutScreen extends StatefulWidget {
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
-  final GlobalKey<ScaffoldMessengerState> _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey =
+      GlobalKey<ScaffoldMessengerState>();
   final TextEditingController _noteController = TextEditingController();
 
   late bool _isLoggedIn;
   late List<CartModel?> _cartList;
   List<Branches>? _branches = [];
 
-
   @override
   void initState() {
     super.initState();
 
-    final CheckoutProvider checkoutProvider = Provider.of<CheckoutProvider>(context, listen: false);
+    final CheckoutProvider checkoutProvider =
+        Provider.of<CheckoutProvider>(context, listen: false);
 
-    _isLoggedIn = Provider.of<AuthProvider>(context, listen: false).isLoggedIn();
-    _branches = Provider.of<SplashProvider>(context, listen: false).configModel?.branches;
+    _isLoggedIn =
+        Provider.of<AuthProvider>(context, listen: false).isLoggedIn();
+    _branches = Provider.of<SplashProvider>(context, listen: false)
+        .configModel
+        ?.branches;
 
-
-    if(_isLoggedIn) {
-      Provider.of<AddressProvider>(context, listen: false).initAddressList().then((value) {
-        CheckOutHelper.selectDeliveryAddressAuto(orderType: widget.orderType, isLoggedIn: _isLoggedIn, lastAddress: null);
+    if (_isLoggedIn) {
+      Provider.of<AddressProvider>(context, listen: false)
+          .initAddressList()
+          .then((value) {
+        CheckOutHelper.selectDeliveryAddressAuto(
+            orderType: widget.orderType,
+            isLoggedIn: _isLoggedIn,
+            lastAddress: null);
       });
       Provider.of<CheckoutProvider>(context, listen: false).clearPrevData();
 
       _cartList = [];
 
-      if(widget.fromCart) {
-        _cartList.addAll(Provider.of<CartProvider>(context, listen: false).cartList);
-      }else{
+      if (widget.fromCart) {
+        _cartList
+            .addAll(Provider.of<CartProvider>(context, listen: false).cartList);
+      } else {
         _cartList.addAll(widget.cartList ?? []);
       }
 
-      if(Provider.of<ProfileProvider>(context, listen: false).userInfoModel != null) {
+      if (Provider.of<ProfileProvider>(context, listen: false).userInfoModel !=
+          null) {
         Provider.of<ProfileProvider>(context, listen: false).getUserInfo();
       }
     }
@@ -93,138 +104,156 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       freeDeliveryType: '',
       amount: widget.amount,
       placeOrderDiscount: 0,
-      couponCode: widget.couponCode, orderNote: null,
+      couponCode: widget.couponCode,
+      orderNote: null,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    final ConfigModel configModel = Provider.of<SplashProvider>(context, listen: false).configModel!;
+    final ConfigModel configModel =
+        Provider.of<SplashProvider>(context, listen: false).configModel!;
     bool kmWiseCharge = configModel.deliveryManagement!.status!;
     bool selfPickup = widget.orderType == 'self_pickup';
 
     return Scaffold(
       key: _scaffoldKey,
       appBar: CustomAppBarWidget(title: getTranslated('checkout', context)),
-      body: _isLoggedIn ? Consumer<CheckoutProvider>(
-        builder: (context, checkoutProvider, child) {
-          double deliveryCharge = checkoutProvider.distance * (configModel.deliveryManagement?.shippingPerKm ?? 1);
-          if(deliveryCharge < (configModel.deliveryManagement?.minShippingCharge ?? 0)) {
-            deliveryCharge = configModel.deliveryManagement?.minShippingCharge ?? 0;
-          }
-          if(!kmWiseCharge || checkoutProvider.distance == -1) {
-            deliveryCharge = 0;
-          }
+      body: _isLoggedIn
+          ? Consumer<CheckoutProvider>(
+              builder: (context, checkoutProvider, child) {
+                double deliveryCharge = checkoutProvider.distance *
+                    (configModel.deliveryManagement?.shippingPerKm ?? 1);
+                if (deliveryCharge <
+                    (configModel.deliveryManagement?.minShippingCharge ?? 0)) {
+                  deliveryCharge =
+                      configModel.deliveryManagement?.minShippingCharge ?? 0;
+                }
+                if (!kmWiseCharge || checkoutProvider.distance == -1) {
+                  deliveryCharge = 0;
+                }
 
-
-          return Consumer<AddressProvider>(
-            builder: (context, address, child) {
-              return Column(children: [
-                Expanded(child: CustomScrollView(slivers: [
-                  SliverToBoxAdapter(child: Center(child: SizedBox(
-                    width: Dimensions.webScreenWidth,
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                      CustomWebTitleWidget(title: getTranslated('checkout', context)),
-
-                      if(!ResponsiveHelper.isDesktop(context)) MapViewWidget(isSelfPickUp: selfPickup),
-
-                      if(!ResponsiveHelper.isDesktop(context)) DetailsViewWidget(
-                        amount: widget.amount ?? 0,
-                        kmWiseCharge: kmWiseCharge,
-                        selfPickup: selfPickup,
-                        deliveryCharge: deliveryCharge,
-                        orderNoteController: _noteController,
-                        orderType: widget.orderType,
-                        cartList: _cartList,
-                      ),
-
-                      if(ResponsiveHelper.isDesktop(context)) Padding(
-                        padding: const EdgeInsets.symmetric(vertical: Dimensions.paddingSizeLarge),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                           if(!selfPickup || (selfPickup && (_branches?.length ?? 0) < 1)) Expanded(flex: 6, child: Container(
-                              decoration: BoxDecoration(
-                                  color: Theme.of(context).cardColor,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color:Theme.of(context).shadowColor,
-                                      blurRadius: 10,
-                                    )
-                                  ]
-                              ),
-                              child: MapViewWidget(isSelfPickUp: selfPickup),
-                            )),
-                            const SizedBox(width: Dimensions.paddingSizeLarge),
-
-                            Expanded(flex: 3, child: Container(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              decoration: BoxDecoration(
-                                  color: Theme.of(context).cardColor,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color:Theme.of(context).shadowColor,
-                                      blurRadius: 10,
-                                    )
-                                  ]
-                              ),
-                              child: DetailsViewWidget(
-                                amount: widget.amount ?? 0, kmWiseCharge: kmWiseCharge,
-                                selfPickup: selfPickup, deliveryCharge: deliveryCharge,
-                                orderNoteController: _noteController,
-                                orderType: widget.orderType ?? '',
-                                cartList: _cartList,
-                              ),
-                            )),
-
-                          ],
+                return Consumer<AddressProvider>(
+                  builder: (context, address, child) {
+                    return Column(children: [
+                      Expanded(
+                          child: CustomScrollView(slivers: [
+                        SliverToBoxAdapter(
+                            child: Center(
+                                child: SizedBox(
+                          width: Dimensions.webScreenWidth,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CustomWebTitleWidget(
+                                    title: getTranslated('checkout', context)),
+                                if (!ResponsiveHelper.isDesktop(context))
+                                  MapViewWidget(isSelfPickUp: selfPickup),
+                                if (!ResponsiveHelper.isDesktop(context))
+                                  DetailsViewWidget(
+                                    amount: widget.amount ?? 0,
+                                    kmWiseCharge: kmWiseCharge,
+                                    selfPickup: selfPickup,
+                                    deliveryCharge: deliveryCharge,
+                                    orderNoteController: _noteController,
+                                    orderType: widget.orderType,
+                                    cartList: _cartList,
+                                  ),
+                                if (ResponsiveHelper.isDesktop(context))
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: Dimensions.paddingSizeLarge),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        if (!selfPickup ||
+                                            (selfPickup &&
+                                                (_branches?.length ?? 0) < 1))
+                                          Expanded(
+                                              flex: 6,
+                                              child: Container(
+                                                decoration: BoxDecoration(
+                                                    color: Theme.of(context)
+                                                        .cardColor,
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10),
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Theme.of(context)
+                                                            .shadowColor,
+                                                        blurRadius: 10,
+                                                      )
+                                                    ]),
+                                                child: MapViewWidget(
+                                                    isSelfPickUp: selfPickup),
+                                              )),
+                                        const SizedBox(
+                                            width: Dimensions.paddingSizeLarge),
+                                        Expanded(
+                                            flex: 3,
+                                            child: Container(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 10),
+                                              decoration: BoxDecoration(
+                                                  color: Theme.of(context)
+                                                      .cardColor,
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                  boxShadow: [
+                                                    BoxShadow(
+                                                      color: Theme.of(context)
+                                                          .shadowColor,
+                                                      blurRadius: 10,
+                                                    )
+                                                  ]),
+                                              child: DetailsViewWidget(
+                                                amount: widget.amount ?? 0,
+                                                kmWiseCharge: kmWiseCharge,
+                                                selfPickup: selfPickup,
+                                                deliveryCharge: deliveryCharge,
+                                                orderNoteController:
+                                                    _noteController,
+                                                orderType:
+                                                    widget.orderType ?? '',
+                                                cartList: _cartList,
+                                              ),
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                              ]),
+                        ))),
+                        const FooterWebWidget(footerType: FooterType.sliver),
+                      ])),
+                      if (!ResponsiveHelper.isDesktop(context))
+                        PlaceOrderButtonView(
+                          deliveryCharge: deliveryCharge,
+                          amount: widget.amount,
+                          cartList: _cartList,
+                          kmWiseCharge: kmWiseCharge,
+                          orderNote: _noteController.text,
+                          orderType: widget.orderType,
                         ),
-                      ),
-
-                    ]),
-                  ))),
-
-                  const FooterWebWidget(footerType: FooterType.sliver),
-
-
-                ])),
-
-                if(!ResponsiveHelper.isDesktop(context)) PlaceOrderButtonView(
-                  deliveryCharge: deliveryCharge,
-                  amount: widget.amount,
-                  cartList: _cartList,
-                  kmWiseCharge: kmWiseCharge,
-                  orderNote: _noteController.text,
-                  orderType: widget.orderType,
-                ),
-
-              ]);
-
-            },
-          );
-        },
-      ) : const NotLoggedInScreen(),
+                    ]);
+                  },
+                );
+              },
+            )
+          : const NotLoggedInScreen(),
     );
   }
 
-
-
-
-  Future<Uint8List> convertAssetToUnit8List(String imagePath, {int width = 50}) async {
+  Future<Uint8List> convertAssetToUnit8List(String imagePath,
+      {int width = 50}) async {
     ByteData data = await rootBundle.load(imagePath);
-    Codec codec = await instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    Codec codec = await instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
     FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ImageByteFormat.png))!.buffer.asUint8List();
+    return (await fi.image.toByteData(format: ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
   }
-
 }
-
-
-
-
-
-
-
-
